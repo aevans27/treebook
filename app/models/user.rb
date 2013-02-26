8eb80d9b048e5299135840b4760bc4d973880e14
Allan Evans <oneblankpage27@gmail.com>
2013-02-25 18:48:54 -0700

    Added status validations and tests, along with other basic design changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                            uniqueness: true,
+                            format: {
+                              with: /a-zA-Z0-9_-/,
+                              message: 'Must be formatted properly.'
+                            }
   # attr_accessible :title, :body
   has_many :statuses
 

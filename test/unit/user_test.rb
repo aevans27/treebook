ffa601be918d473d9eca00afab737c71e3a0092f
Allan Evans <oneblankpage27@gmail.com>
2013-02-25 20:05:47 -0700

    Generated profile controller

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -26,11 +26,21 @@ require 'test_helper'
 	end	 
 
 	test "a user should have a profile name without spaces" do
-	user = User.new
+	user = User.new(first_name: 'Allan', last_name: 'Evans', email: 'oneblankpage27@gmail.com')
+	user.password = user.password_confirmation = 'trigger0'
+	
 	user.profile_name = "My profile with spaces"
 
 	assert !user.save
 	assert !user.errors[:profile_name].empty?
 	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Allan', last_name: 'Evans', email: 'oneblankpage27@gmail.com')
+		user.password = user.password_confirmation = 'trigger0'
+
+		user.profile_name = 'allanevans_1'
+		assert user.valid?
 	end
 end
